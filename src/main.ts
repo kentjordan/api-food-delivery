7808797608630bbc0fec787954045f63253f6d38
Kent Jordan <kentaki.dev@gmail.com>
2023-12-03 22:04:05 +0800

    Fixes: BigInt

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
+
 import 'dotenv/config'
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
