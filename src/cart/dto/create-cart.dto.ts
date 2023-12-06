db3347739d12a56442d418551626fcd06871250d
Kent Jordan <kentaki.dev@gmail.com>
2023-12-06 20:34:35 +0800

    Changed column name into cart_item_instruction_id

--- src/cart/dto/create-cart.dto.ts
+++ src/cart/dto/create-cart.dto.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const createCartScheme = z.object({
-    item_instruction: z.string().optional(),
+    cart_item_instruction_id: z.string().optional(),
     product_extra_id: z.string().uuid().optional(),
     product_id: z.string().uuid(),
     user_id: z.string().uuid(),
